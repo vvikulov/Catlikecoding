e1e9315cfd5f512edf58179a4139b0a35d7a172e
viav <viav@bk.ru>
2019-10-12 23:50:18 +0300

    - Mathematical Surfaces

---
+++ Assets/MathSurfacesProject/GraphFunctionName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GraphG {
+    public enum GraphFunctionName {
+        Sine,
+        Sine2D,
+        MultiSine,
+        MultiSine2D,
+        Ripple,
+        Cylinder,
+        Sphere,
+        Torus
+    }
+}
