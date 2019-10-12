c2ab2304e6337e62fdc040cf6aa2d2e38205da6b
viav <viav@bk.ru>
2019-10-12 13:42:29 +0300

    - new Unity version - Graph is in progress

---
+++ Assets/GraphProject/Scripts/Graph.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GraphG
+{
+    public class Graph : MonoBehaviour
+    {
+        #region fields
+        [SerializeField]
+        protected Transform m_pointPrefab;
+        #endregion
+
+        #region Unity Events
+        private void Awake()
+        {
+            Vector3 scale = Vector3.one / 5.0f;
+            Vector3 position;
+            for(int i = 0; i < 10; i++)
+            {
+                Transform point = GameObject.Instantiate(m_pointPrefab, this.transform, false);
+                point.localPosition = Vector3.right * ((i + 0.5f) / 5.0f - 1.0f);
+                point.localScale = scale;
+            }
+        }
+        #endregion
+    }
+}
