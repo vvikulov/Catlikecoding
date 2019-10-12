b93f62a764f148fd975b95218702f40a96f265a1
viav <viav@bk.ru>
2019-10-12 22:12:42 +0300

    - Graph (Animated sine function)

--- Assets/GraphProject/Scripts/Graph.cs
+++ Assets/GraphProject/Scripts/Graph.cs
@@ -9,18 +9,39 @@ namespace GraphG
         #region fields
         [SerializeField]
         protected Transform m_pointPrefab;
+        [SerializeField]
+        [Range(10, 100)]
+        protected int m_resolution = 10;
+
+        private Transform[] m_points;
         #endregion
 
         #region Unity Events
         private void Awake()
         {
-            Vector3 scale = Vector3.one / 5.0f;
+            float step = 2f / m_resolution;
+            Vector3 scale = Vector3.one * step;
             Vector3 position;
-            for(int i = 0; i < 10; i++)
+            position.y = 0.0f;
+            position.z = 0.0f;
+
+            m_points = new Transform[m_resolution];
+            for (int i = 0; i < m_points.Length; i++)
             {
                 Transform point = GameObject.Instantiate(m_pointPrefab, this.transform, false);
-                point.localPosition = Vector3.right * ((i + 0.5f) / 5.0f - 1.0f);
+                position.x = (i + 0.5f) * step - 1.0f;
+                point.localPosition = position;
                 point.localScale = scale;
+                m_points[i] = point;
+            }
+        }
+
+        private void Update() {
+            for (int i = 0; i < m_points.Length; i++) {
+                Transform point = m_points[i];
+                Vector3 position = point.localPosition;
+                position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+                point.localPosition = position;
             }
         }
         #endregion
